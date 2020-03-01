5e4cfe851ef948449359e00fe3acd0d03cb767a8
Karol <woykar98@gmail.com>
2020-03-01 19:54:18 +0100

    implement main main function

--- temp.py
+++ temp.py
@@ -3,7 +3,30 @@ y = 'fight'
 
 z1 = 'brave'
 
-if z1 == 'coward':
+if z1=='coward':
     print(x)
-elif z1 != 'coward':
+elif z1!='coward':
     print(y)
+
+
+def main():
+    password = input("Please enter your password::")
+    if password=="student":
+        student()
+    elif password=="teacher":
+        teacher()
+    elif password=="headmaster":
+        headmaster()
+    elif password=="janitor":
+        janitor()
+    else:
+        print("incorrect password")
+
+def student():
+    print("welcome student")
+def teacher():
+    print("welcome teacher")
+def headmaster():
+    print("welcome headmaster")
+def janitor():
+    print("welcome janitor")
